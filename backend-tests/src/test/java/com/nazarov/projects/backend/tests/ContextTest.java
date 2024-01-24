f43984b39906aba48b70a9a8e14f062974c53e9d
Andrii Nazarov <andreu.nazarov@gmail.com>
2024-01-24 18:33:15 +0100

    Added log to Allure Report

--- backend-tests/src/test/java/com/nazarov/projects/backend/tests/ContextTest.java
+++ backend-tests/src/test/java/com/nazarov/projects/backend/tests/ContextTest.java
@@ -4,12 +4,15 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.nazarov.projects.backend.services.blog.BlogService;
 import com.nazarov.projects.backend.tests.config.TestConfig;
+import com.nazarov.projects.backend.tests.extenssions.LogExt;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 @SpringJUnitConfig(TestConfig.class)
+@ExtendWith(LogExt.class)
 @Slf4j
 class ContextTest {
 
@@ -20,6 +23,5 @@ class ContextTest {
   void testContext() {
     assertThat(blogService).isNotNull();
   }
-
 
 }
---
+++ backend-tests/src/test/java/com/nazarov/projects/backend/tests/extenssions/LogExt.java
@@ -0,0 +1,49 @@
+package com.nazarov.projects.backend.tests.extenssions;
+
+import static java.util.Objects.nonNull;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import com.nazarov.projects.commons.AllureLogAppender;
+import io.qameta.allure.Allure;
+import java.io.ByteArrayInputStream;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+@Slf4j
+public class LogExt implements BeforeEachCallback, AfterEachCallback {
+
+  private static final Function<ILoggingEvent, String> eventFormatter = event -> {
+    var formatter = DateTimeFormatter.ofPattern("HH:mm:ss.SSS").withZone(ZoneId.systemDefault());
+
+    return String.format("[%s] [%s] [%s] - %s",
+        formatter.format(event.getInstant()),
+        event.getLevel(),
+        event.getLoggerName(),
+        event.getFormattedMessage());
+  };
+
+  @Override
+  public void beforeEach(ExtensionContext extensionContext) {
+    AllureLogAppender.clearEvents();
+  }
+
+  @Override
+  public void afterEach(ExtensionContext extensionContext) {
+    List<ILoggingEvent> events = AllureLogAppender.getEvents();
+    byte[] log = "Log output is empty".getBytes();
+    if (nonNull(events)) {
+      log = events.stream()
+          .map(eventFormatter)
+          .collect(Collectors.joining(System.lineSeparator(), "", System.lineSeparator()))
+          .getBytes();
+    }
+    Allure.addAttachment("log", "text/plain", new ByteArrayInputStream(log), ".log");
+  }
+}
